0c829b9910f20393ae3d957964fa90956cc4473e
jeremiahlukus <jeremiahlukus1@gmail.com>
2018-10-07 13:20:54 -0400

    Added better scrolling

--- product_listing/lib/main.dart
+++ product_listing/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/rendering.dart';
 import './product_manager.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override
